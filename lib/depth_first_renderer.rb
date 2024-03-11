286c1b8af50ea2242e9fb3ba79f6c66bcd6adfea
Julik Tarkhanov <me@julik.nl>
2024-03-11 12:40:39 +0100

    Cache every node when rendering depth-first

--- lib/depth_first_renderer.rb
+++ lib/depth_first_renderer.rb
@@ -2,23 +2,23 @@ require "stringio"
 
 class DepthFirstRenderer
   def node_to_fragments(node, cache_store)
+    if fragments = cache_store.read(node.cache_key)
+      return fragments
+    end
+
     if node.children.empty?
       ["<#{node.name} id=#{node.id} />"]
     else
       child_fragments = node.children.map do |child|
-        if (cached = cache_store.read(child.cache_key))
-          cached
-        else
-          node_to_fragments(child, cache_store).tap do |f|
-            cache_store.write(child.cache_key, f) # We know all fragments have been expanded
-          end
-        end
+        node_to_fragments(child, cache_store)
       end
       [
         "<#{node.name} id=#{node.id}>",
         child_fragments,
         "</#{node.name}>"
       ]
+    end.tap do |fragments|
+      cache_store.write(node.cache_key, fragments)
     end
   end
 end
